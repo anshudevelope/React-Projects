7208b678c58110526658e4fbf388701cf6d643d4
Anshu Maurya <anshu.maurya7300@gmail.com>
2025-03-26 08:56:53 +0000

    button added

--- keep-record/src/App.js
+++ keep-record/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Header from './Components/Header';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <div>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <Button variant="contained" color="success">Add</Button>
+      </div>
     </div>
   );
 }
