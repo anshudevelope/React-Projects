f4b890bd66c5482a73d180cf4986ed95d2dcb2a0
Anshu Maurya <anshu.maurya7300@gmail.com>
2025-04-01 08:16:12 +0000

    create done

--- studentrecord/src/CreateStudentData.jsx
+++ studentrecord/src/CreateStudentData.jsx
@@ -9,6 +9,8 @@ const CreateStudentData = () => {
   const [place, setPlace] = useState("");
   const [phone, setPhone] = useState("");
 
+  const [validation, setValidation] = useState(false);
+
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
@@ -37,13 +39,20 @@ const CreateStudentData = () => {
       <h2>Add New Student</h2>
       <form onSubmit={handleSubmit}>
         <label htmlFor='id'>Id</label>
-        <input type='text' id='id' name='id' value={id} onChange={e => setId(e.target.value)}></input>
+        <input type='text' id='id' name='id' required value={id} onChange={e => setId(e.target.value)} onMouseDown={() => setValidation(true)}></input>
+        {id.length == 0 && validation && <span className='errorMsg'>Please enter your Id</span>}
+
         <label htmlFor='name'>Name</label>
-        <input type='text' id='name' name='name' value={name} onChange={e => setName(e.target.value)}></input>
+        <input type='text' id='name' name='name' required value={name} onChange={e => setName(e.target.value)} onMouseDown={() => setValidation(true)}></input>
+        {name.length == 0 && validation && <span className='errorMsg'>Please enter your name</span>}
+
         <label htmlFor='place'>Place</label>
-        <input type='text' id='place' name='place' value={place} onChange={e => setPlace(e.target.value)}></input>
+        <input type='text' id='place' name='place' required value={place} onChange={e => setPlace(e.target.value)} onMouseDown={() => setValidation(true)}></input>
+        {place.length == 0 && validation && <span className='errorMsg'>Please enter your place</span>}
+
         <label htmlFor='phone'>Phone</label>
-        <input type='number' id='phone' name='phone' value={phone} onChange={e => setPhone(e.target.value)}></input>
+        <input type='number' id='phone' name='phone' required value={phone} onChange={e => setPhone(e.target.value)} onMouseDown={() => setValidation(true)}></input>
+        {phone.length == 0 && validation && <span className='errorMsg'>Please enter your phone</span>}
       
 
       <div>
