60b6a7cc7111bcfa4d77d06b7086ccd7cfa587a3
Anshu Maurya <anshu.maurya7300@gmail.com>
2025-04-04 12:34:52 +0000

    Login & Signup form created

--- React-hook-form/src/Login.jsx
+++ React-hook-form/src/Login.jsx
@@ -1,7 +1,45 @@
+import { useState } from "react"
+import './App.css'
 
-function Login(){
-    return(
-        <div>Hello Login</div>
+
+function Login() {
+
+    const [isLogin, setIsLogin] = useState(true);
+
+
+
+    return (
+        <div className="container">
+            <div className="form-container">
+                <div className="form-toggle">
+                    <button className={isLogin ? 'active' : ""} onClick={() => setIsLogin(true)}>Login</button>
+                    <button className={!isLogin ? 'active' : ""} onClick={() => setIsLogin(false)}>Signup</button>
+                </div>
+                {isLogin ? <>
+
+                    <div className="form">
+                        <h2>Login Form</h2>
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <a href="#">Forgot Password</a>
+                        <button>Login</button>
+                        <p>Not a member <a href="#" onClick={() => setIsLogin(false)}>Sign up</a></p>
+                    </div>
+                </> :
+                    <>
+
+                        <div className="form">
+                            <h2>Login Form</h2>
+                            <input type="email" placeholder="Email" />
+                            <input type="password" placeholder="Password" />
+                            <input type="password" placeholder="Confirm Password" />
+                            <button>Sign up</button>
+                            <p>Already a memeber<a href="#" onClick={() => setIsLogin(true)}> Sign In</a></p>
+                        </div>
+
+                    </>}
+            </div>
+        </div>
     )
 }
 
--- React-hook-form/src/main.jsx
+++ React-hook-form/src/main.jsx
@@ -4,9 +4,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import './index.css'
 import App from './App.jsx'
+import Login from './Login.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Login />
   </StrictMode>,
 )
